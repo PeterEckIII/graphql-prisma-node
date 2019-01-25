4cdaf471149f09923787b6057ae014f67e7c74c1
Peter Eck <jpeckiii@gmail.com>
2019-01-25 11:57:57 -0600

    Clean up index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,42 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-const typeDefs = `
-type Query {
-    info: String!
-}
-`;
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+  }
+];
 
+let idCount = links.length;
 const resolvers = {
   Query: {
-    info: () => `This is the Hackernews Clone API`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links
+  },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      };
+      links.push(link);
+      return link;
+    },
+    updateLink: (parent, args, id) => {
+      const link = {
+        id: id,
+        url: args.url,
+        description: args.description
+      };
+    }
   }
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: "./src/schema.graphql",
   resolvers
 });
 
-server.start(() => console.log(`Server is running on https://localhost:4000`));
+server.start(() => console.log(`Server has started on port 4000`));
