0b9e6aee56c55c52847d1c306bf8bf9c26be1322
Peter Eck <jpeckiii@gmail.com>
2019-01-27 17:41:56 -0600

    Add vote mutation

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -31,4 +31,19 @@ const post = (root, args, context) => {
   });
 };
 
-module.exports = { signup, login, post };
+const vote = async (root, args, context, info) => {
+  const userId = getUserId(context);
+  const linkExists = await context.prisma.$exists.vote({
+    user: { id: userId },
+    link: { id: args.linkId }
+  });
+  if (linkExists) {
+    throw new Error(`Already voted for link: ${args.linkId}`);
+  }
+  return context.prisma.createVote({
+    user: { connect: { id: userId } },
+    link: { connect: { id: args.linkId } }
+  });
+};
+
+module.exports = { signup, login, post, vote };
